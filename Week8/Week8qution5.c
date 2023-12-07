079d009fa37282a03a52359740118214ccac149c
noob <yinxiehuang@gmail.com>
2023-12-07 17:01:43 +0800

    unpload

--- Week8/Week8qution5.c
+++ Week8/Week8qution5.c
@@ -3,6 +3,24 @@
 #include <stdlib.h>
 
 #define INBORAD(y,x) (x) < 19 && (y) < 19 && (x) >= 0 && (y) >= 0
+#define ISWINER(t) ({for( int k = 0; k < 8; k++ ){\
+                    int x = j + Direction[k][1], y = i + Direction[k][0], time = 4;\
+                    while( abs(x - j) <= 4 && abs(y - i) <= 4){\
+                        if( INBORAD(y,x) && board[y][x] == t){\
+                            time--;\
+                        }\
+                        x += Direction[k][1];\
+                        y += Direction[k][0];\
+                    }\
+                    if(time == 0 ){\
+                        if(t == 'O'){\
+                            printf("White\n");\
+                        }else{\
+                            printf("Black\n");\
+                        }\
+                        return 0;\
+                    }\
+                  }})
 int Direction[8][2] = {{1,1},{1,0},{1,-1},{0,1},{0,-1},{-1,1},{-1,0},{-1,-1}};
 
 
@@ -14,35 +32,9 @@ int main(){
     for( int i = 0; i < 19; i++ ){
         for( int j = 0; j < 19; j++ ){
             if(board[i][j] == 'O'){
-                for( int k = 0; k < 8; k++ ){
-                    int x = j + Direction[k][1], y = i + Direction[k][0], time = 4;
-                    while( abs(x - j) <= 4 && abs(y - i) <= 4){
-                        if( INBORAD(y,x) && board[y][x] == 'O'){
-                            time--;
-                        }          
-                        x += Direction[k][1];
-                        y += Direction[k][0];
-                    }
-                    if(time == 0 ){
-                        printf("White\n");
-                        return 0;
-                    }
-                }
+                ISWINER('O');
             }else if( board[i][j] == 'X'){
-                for( int k = 0; k < 8; k++ ){
-                    int x = j + Direction[k][1], y = i + Direction[k][0], time = 4;
-                    while( abs(x - j) <= 4 && abs(y - i) <= 4){
-                        if( INBORAD(y,x) && board[y][x] == 'X'){
-                            time--;
-                        }          
-                        x += Direction[k][1];
-                        y += Direction[k][0];
-                    }
-                    if(time == 0 ){
-                        printf("Black\n");
-                        return 0;
-                    }
-                }
+                ISWINER('X');
             }
         }
     }
